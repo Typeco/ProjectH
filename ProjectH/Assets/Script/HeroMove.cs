32ffe2d5d5a383de36f4c12771f8439179c854ef
Typeco <40288523+Typeco@users.noreply.github.com>
2020-08-01 03:51:47 +0800

    修改角色触发动画逻辑
    只有当角色移动时才会播放动画

--- ProjectH/Assets/Script/HeroMove.cs
+++ ProjectH/Assets/Script/HeroMove.cs
@@ -7,12 +7,13 @@ public class HeroMove : MonoBehaviour
 
     public float moveSpeed = 3;
 
-
+    private Animator ant;
+    private Transform tf;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ant = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,9 +22,29 @@ public class HeroMove : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         transform.Translate(Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
 
+
+        if (h == 0)
+        {
+            ant.speed = 0;
+        }
+        else if(h > 0)
+        {
+            ant.speed = 1;
+        }
+        else if (h < 0)
+        {
+            ant.speed = 1;
+        }
+
+
         float v = Input.GetAxisRaw("Vertical");
         transform.Translate(Vector3.up * v * moveSpeed * Time.deltaTime, Space.World);
 
+        
+        if (v != 0)
+        {
+            ant.speed = 1;
+        }
 
     }
 }
