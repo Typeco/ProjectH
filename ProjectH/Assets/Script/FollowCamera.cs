3fa1d08858ca92f5cf299995a9c0c02aa1346c00
Typeco <40288523+Typeco@users.noreply.github.com>
2020-08-02 00:02:56 +0800

    mainsence和女主左右移动动画
    文件路径重新整理

--- ProjectH/Assets/Script/FollowCamera.cs
+++ ProjectH/Assets/Script/FollowCamera.cs
@@ -21,7 +21,7 @@ public class FollowCamera : MonoBehaviour
         //transform.position = offset + player.position;
 
         // X Z都在移动范围（可以随便移动）
-        if (player.position.x>-7 && player.position.x<7 && player.position.z > 4)
+        if (player.position.x > -7 && player.position.x < 7 && player.position.z > 4)
         {
             transform.position = offset + player.position;
         }
@@ -29,15 +29,13 @@ public class FollowCamera : MonoBehaviour
         else if ((player.position.x > -7 && player.position.x < 7) && player.position.z < 4)
         {
             transform.position = new Vector3(offset.x + player.position.x, transform.position.y, transform.position.z);
-
             //transform.position = offset + player.position;
         }
         // Z 在移动范围，X不在（仅Z 可以随便移动）
-        else if ((player.position.x < -7 && player.position.x > 7) && player.position.z > 4)
+        else if ((player.position.x < -7 || player.position.x > 7) && player.position.z > 4)
         {
             //transform.forward = offset + player.position;
             transform.position = new Vector3( transform.position.x, transform.position.y,offset.z + player.position.z);
-
         }
 
 
