76777c987f8b1257f1953fca08bfde768363a2d7
Typeco <40288523+Typeco@users.noreply.github.com>
2020-08-01 23:17:40 +0800

    镜头移动脚本
    镜头跟随人物可在固定区域内移动

--- ProjectH/Assets/Script/HeroMove.cs
+++ ProjectH/Assets/Script/HeroMove.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,24 +10,64 @@ public class HeroMove : MonoBehaviour
 
     private Animator ant;
 
+    Rigidbody rigid;
+
     // Start is called before the first frame update
     void Start()
     {
         ant = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //X轴方向输入
         float h = Input.GetAxisRaw("Horizontal");
-        transform.Translate(Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
-
+        //transform.Translate(Vector3.right * h * moveSpeed * Time.fixedDeltaTime, Space.World);
+        //用h判断动画播放正反
         ant.SetInteger("h", (int)h);
-
+        //if (h != 0)return;
+        //Z轴方向输入
         float v = Input.GetAxisRaw("Vertical");
-        transform.Translate(Vector3.forward * v * moveSpeed * Time.deltaTime, Space.World);
-
+        //transform.Translate(Vector3.forward * v * moveSpeed * Time.fixedDeltaTime, Space.World);
+        //v判断动画
         ant.SetInteger("v", (int)v);
+
+
+        //设置移动方向的速度（当斜方向移动时 移动速度需要开根号）
+        if (v == 1 && h ==1)
+        {
+            double moveZ = 1 / Math.Sqrt(v * v + h * h);
+            transform.Translate(Vector3.right * (float)moveZ * moveSpeed * Time.fixedDeltaTime, Space.World);
+            transform.Translate(Vector3.forward * v * (float)moveZ * moveSpeed * Time.fixedDeltaTime, Space.World);
+        }
+        else if (v == -1 && h ==1)
+        {
+            double moveZ = 1 / Math.Sqrt(v * v + h * h);
+            transform.Translate(Vector3.right * (float)moveZ * moveSpeed * Time.fixedDeltaTime, Space.World);
+            transform.Translate(Vector3.forward * -(float)moveZ * moveSpeed * Time.fixedDeltaTime, Space.World);
+        }
+        else if (v == 1 && h == -1)
+        {
+            double moveZ = 1 / Math.Sqrt(v * v + h * h);
+            transform.Translate(Vector3.right * -(float)moveZ * moveSpeed * Time.fixedDeltaTime, Space.World);
+            transform.Translate(Vector3.forward *(float)moveZ * moveSpeed * Time.fixedDeltaTime, Space.World);
+        }
+        else if (v == -1 && h ==-1)
+        {
+            double moveZ = 1 / Math.Sqrt(v * v + h * h);
+            transform.Translate(Vector3.right * -(float)moveZ * moveSpeed * Time.fixedDeltaTime, Space.World);
+            transform.Translate(Vector3.forward * -(float)moveZ * moveSpeed * Time.fixedDeltaTime, Space.World);
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * v * moveSpeed * Time.fixedDeltaTime, Space.World);
+            transform.Translate(Vector3.right * h * moveSpeed * Time.fixedDeltaTime, Space.World);
+
+        }
+
 
     }
 }
