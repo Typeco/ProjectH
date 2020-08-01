7d3fd2607d19d62818ed712400e8782f2da8f91e
Typeco <40288523+Typeco@users.noreply.github.com>
2020-08-01 16:07:36 +0800

    脚本控制Knight移动和转向
    RT

--- ProjectH/Assets/Script/HeroMove.cs
+++ ProjectH/Assets/Script/HeroMove.cs
@@ -8,7 +8,6 @@ public class HeroMove : MonoBehaviour
     public float moveSpeed = 3;
 
     private Animator ant;
-    private Transform tf;
 
     // Start is called before the first frame update
     void Start()
@@ -22,29 +21,12 @@ public class HeroMove : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         transform.Translate(Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
 
-
-        if (h == 0)
-        {
-            ant.speed = 0;
-        }
-        else if(h > 0)
-        {
-            ant.speed = 1;
-        }
-        else if (h < 0)
-        {
-            ant.speed = 1;
-        }
-
+        ant.SetInteger("h", (int)h);
 
         float v = Input.GetAxisRaw("Vertical");
         transform.Translate(Vector3.up * v * moveSpeed * Time.deltaTime, Space.World);
 
-        
-        if (v != 0)
-        {
-            ant.speed = 1;
-        }
+        ant.SetInteger("v", (int)v);
 
     }
 }
