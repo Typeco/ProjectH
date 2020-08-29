18fc981b6fb70d5e9e261fa60082610e76a9f0e5
Typeco <40288523+Typeco@users.noreply.github.com>
2020-08-30 01:08:21 +0800

    删除垃圾，上传新代码

---
+++ ProjectH/Assets/Script/ColliderControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderControl : MonoBehaviour
+{ 
+    [Header("碰撞盒返回值")]
+    public bool InConllider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        
+        switch (collider.tag)
+        {
+            case ("TipsConllider"):
+                InConllider = true;
+                break;
+            case ("ImgConllider"):
+                break;
+            default:
+                break;
+        }
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        switch (collider.tag)
+        {
+            case ("TipsConllider"):
+                InConllider = false;
+                break;
+            case ("ImgConllider"):               
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
---
+++ ProjectH/Assets/Script/UIControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    [Header("UI Object")]
+    public GameObject TipsObj;
+    public GameObject ImgObj;
+
+    [Header("ColliderControl")]
+    public GameObject Player;
+
+
+
+    bool NeedImg;
+    private void Update()
+    {
+        TipsControl();
+        //ImgControl();
+
+    }
+
+    //Tips范围检测
+    void TipsControl()
+    {
+        NeedImg = ImgObj.activeSelf;
+        if (Player.GetComponent<ColliderControl>().InConllider && Input.GetKeyDown(KeyCode.E))
+        {
+            ImgObj.SetActive(!NeedImg);
+        }
+        else if (!Player.GetComponent<ColliderControl>().InConllider && Input.GetKeyDown(KeyCode.E))
+        {
+            ImgObj.SetActive(false);
+        }
+
+        if (Player.GetComponent<ColliderControl>().InConllider && NeedImg == false)
+        {
+            TipsObj.SetActive(true);
+        }
+        else
+        {
+            TipsObj.SetActive(false);
+        }
+
+
+    }
+
+
+
+}
