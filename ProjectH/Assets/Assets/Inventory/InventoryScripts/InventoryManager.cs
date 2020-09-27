3b4dbb3776bb185f70b6a5cd5ba53b7e9500aed6
Typeco <40288523+Typeco@users.noreply.github.com>
2020-09-27 22:05:17 +0800

    音频控制代码

---
+++ ProjectH/Assets/Assets/Inventory/InventoryScripts/InventoryManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    static InventoryManager instance;
+
+    public Inventory myBag;
+    public GameObject slotGrid;
+    // public Slot slotPrefab;
+    public GameObject emptySlot;
+    public Text itemInfromation;
+    public GameObject checkMake;
+
+    public List<GameObject> slots = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (instance != null) Destroy(this);
+        instance = this;
+        
+    }
+
+    private void OnEnable()
+    {
+        RefreshItem();
+        instance.itemInfromation.text = "";
+    }
+
+    public static void RefreshItem()
+    {
+        for (int i = 0; i < instance.slotGrid.transform.childCount; i++)
+        {
+            Destroy(instance.slotGrid.transform.GetChild(i).gameObject);
+            instance.slots.Clear();
+        }
+
+        for (int i = 0; i < instance.myBag.itemList.Count; i++)
+        {
+            instance.slots.Add(Instantiate(instance.emptySlot));
+            instance.slots[i].transform.SetParent(instance.slotGrid.transform);
+            instance.slots[i].GetComponent<Slot>().SetupSlot(instance.myBag.itemList[i]);
+        }
+    }
+
+    public static void UpdateItemInfo(string itemDescription)
+    {
+        if (instance.itemInfromation.text == itemDescription)
+        {
+            instance.itemInfromation.text = "";
+            instance.checkMake.SetActive(false);
+        }
+        else
+        {
+            instance.itemInfromation.text = itemDescription;
+            instance.checkMake.SetActive(true);
+        }
+    }
+
+    public static void UpdateCheckMake(Vector3 makePosition)
+    {
+        instance.checkMake.transform.position = makePosition;
+    }
+
+
+
+}
---
+++ ProjectH/Assets/Assets/Script/musicSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class musicSwitch : MonoBehaviour
+{
+    public bool musicOn = true;
+    public void Switch()
+    {
+        musicOn = !musicOn;
+
+        if (!musicOn)
+        {
+            this.GetComponent<Image>().color = Color.red;
+            this.GetComponentInChildren<Text>().text = "Off";
+        }
+        else
+        {
+            this.GetComponent<Image>().color = Color.white;
+            this.GetComponentInChildren<Text>().text = "On";
+        }
+    }
+}
